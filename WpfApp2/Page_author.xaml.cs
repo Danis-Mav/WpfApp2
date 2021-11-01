728e05456af98f4e3d684517c31515a7ec7a0fca
Мавлин Данис Ильдарович <201911@mck-ktits.ru>
2021-11-01 14:51:06 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/Page_author.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page_author.xaml
+    /// </summary>
+    public partial class Page_author : Page
+    {
+        public static ObservableCollection<Employee> userss { get; set; }
+        public Page_author()
+        {
+            InitializeComponent();
+        }
+
+        private void author_event_click(object sender, RoutedEventArgs e)
+        {
+            userss = new ObservableCollection<Employee>(bd_connection.connection.Employee.ToList());
+            var z = userss.Where(a => a.login == txt_Login.Text && a.password == txt_Password.Password).FirstOrDefault();
+            if (z != null)
+            {
+                MessageBox.Show(z.Name);
+                NavigationService.Navigate(new Page_zakaz());
+
+            }
+            else
+            {
+                MessageBox.Show("логин или  пароль не верные", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void Botton_click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page_reg());
+        }
+    }
+}
---
+++ WpfApp2/Page_reg.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page_reg.xaml
+    /// </summary>
+    public partial class Page_reg : Page
+    {
+        public static ObservableCollection<Position> Posit { get; set; }
+        int i { get; set; }
+        public Page_reg()
+        {
+            InitializeComponent();
+            Posit = new ObservableCollection<Position>(bd_connection.connection.Position.ToList());
+            this.DataContext = this;
+
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            App.Current.MainWindow.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void Botton_Click(object sender, RoutedEventArgs e)
+        {
+            var a = new Employee();
+            a.Name = name_txt.Text;
+            a.login = login_txt.Text;
+            a.password = password_txt.Text;
+            a.id_position = i;
+            bd_connection.connection.Employee.Add(a);
+            bd_connection.connection.SaveChanges();
+            MessageBox.Show("all ok");
+            NavigationService.GoBack();
+        }
+        private void cmb_position(object sender, SelectionChangedEventArgs e)
+        {
+            var a = (sender as ComboBox).SelectedItem as Position;
+            i = a.id_position;
+        }
+    }
+}
